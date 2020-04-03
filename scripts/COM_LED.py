2e1de06b44f5967f8cf72b023f4bf6cbeacbe017
James Cooper <58943271+jcoopertech@users.noreply.github.com>
2020-04-03 09:13:54 +0100

    Importing and shared variables
    Updated shared variables method. Testing importing.

--- scripts/COM_LED.py
+++ scripts/COM_LED.py
@@ -1,3 +1,21 @@
+#!/usr/bin python3
 # AutoRead common
-def checkimport():
-    print("Hello world!")
+"""This is a common file for controlling GPIO. Don't try to run this"""
+
+def GPIO_ON(Channel):
+    """Turns on the pin."""
+    GPIO.output(Channel, GPIO.HIGH)
+    print("COM_LED.py: Pin {0} is HIGH".format(Channel))
+
+
+def GPIO_OFF(Channel):
+    """Turns off the channel."""
+    GPIO.output(Channel, GPIO.LOW)
+    print("COM_LED.py: Pin {0} is LOW".format(Channel))
+
+
+def setupGPIO(LED_def):
+    GPIO.setmode(GPIO.BOARD)
+    for key, value in LED_def.items():
+        GPIO.setup(value, GPIO.OUT)
+        print("COM_LED: {0} has been set up on PIN {1}".format(key,value))
