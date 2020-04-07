042f945cef740133276f0bb7940d1b55e2a4dd3b
James Cooper <58943271+jcoopertech@users.noreply.github.com>
2020-04-07 19:38:28 +0100

    Updated AR_Track
    After running, sorted suntax and starting to integrate classes properly

--- scripts/AR_track.py
+++ scripts/AR_track.py
@@ -4,31 +4,29 @@
 import math
 import COM_CONFIG
 
-LightTypeObjects = [LightType("TW1", 540, 242, 454), # YOKE OFFSET OF 454 does not account for clamps
-                    LightType("ETC_REV", 540, 270, 713)] # 713 accounts to pipe - but measure it
-
-Lights = [["TW1", (-3500,2000,None), 8],
-          ["TW1", (-3500,2000,None), 8]]
-
+_offline_test_ = True
+if _offline_test_ == True:
+    axisDict = {
+    "8": (6750, 0, 0),
+    }
 
 class Axis():
     def __init__(self, AxisNumber, axisarray):
         self.AxisNumber = AxisNumber
-        self.position = axisDict[AxisNumber][0]
-        self.Y_Coordinate = COM_CONFIG.AxisYValues[self.AxisNumber-1]
+        self.position = axisDict[str(AxisNumber)][0]
+        self.Y_Coordinate = COM_CONFIG.AxisYValues[int(self.AxisNumber)-1]
 
 
 class LightType:
-    def __init__(self, type_name, total_pan, total_tilt, yoke_offset)
+    def __init__(self, type_name, total_pan, total_tilt, yoke_offset):
         self.type_name = type_name
         self.total_pan = total_pan
         self.total_tilt = total_tilt
         self.yoke_offset = yoke_offset
 
 
-
 class Light():
-    def __init__(self,axis,ThisLightType):
+    def __init__(self,axis,ThisLightType, (Base_X,Base_Y,Base_Z)):
         # Calculate maximum pan ranges in degrees.
         self.Axis = axis
         #Use LightType.type_name etc to access type variables
@@ -38,26 +36,43 @@ class Light():
         self.neg_pan = - self.plus_pan
         self.plus_tilt = self.LightType.total_tilt / 2
         self.neg_tilt = - self.plus_tilt
-        self.base_coord = {"X": None, "Y": None, "Z": None}
+        self.base_coord = {"X": Base_X, "Y": Base_Y, "Z": Base_Z}
         self.offset_coord = {"X": self.base_coord["X"],
                              "Y": self.base_coord["Y"],
-                             "Z": self.Axis.position - }
+                             "Z": self.Axis.position - self.LightType.yoke_offset}
+
+
+LightTypeObjects = [LightType("TW1", 540, 242, 454), # YOKE OFFSET OF 454 does not account for clamps
+                    LightType("ETC_REV", 540, 270, 713)] # 713 accounts to pipe - but measure it
+
+# LightObjects = [Light(axis,LightTypeObjects[0])]
+
+Lights = [["TW1", (-3500,2000,None), "8"],
+          ["TW1", (-3500,2000,None), "8"]]
 
 
 def setup_AxisObjects(axisDict):
     AxisObjects = []
-    for key, value in axisDict:
+    print(axisDict)
+    for key, value in axisDict.items():
         AxisObjects.append(Axis(key, value[0]))
     return AxisObjects
 
 
-def setup_LightObjects(AxisObjects, LightTypeObjects):
+def setup_LightObjects(Lights, AxisObjects, LightTypeObjects):
     LightObjects = []
-    for LightObject in LightObjects:
-        LightObjects.append(Light(AxisObjects[],LightTypeObjects[0]))
+    for Light in Lights:
+        # Determine Axis to assign here
+        LightObjects.append(Light(axis, LightTypeObjects[0], ))
     return LightObjects
 
 
 def main_track(axisDict, Lights):
     AxisObjects = setup_AxisObjects(axisDict)
     LightObjects = setup_LightObjects(Lights, AxisObjects, LightTypeObjects)
+
+
+if __name__ == "__main__":
+    if _offline_test_ == True:
+        pass
+    main_track(axisDict, Lights)
